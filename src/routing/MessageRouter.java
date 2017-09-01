fe467cab62a44c62aeecfef2baca404efa50cd2e
Corey MacBook Pro <coreyearleon@icloud.com>
2017-09-01 14:52:08 -0700

    Added updated BubbleWrap code from knightcode/the-one-pitt

--- src/routing/MessageRouter.java
+++ src/routing/MessageRouter.java
@@ -82,15 +82,15 @@ public abstract class MessageRouter {
 	/** Receive return value for unspecified reason */
 	public static final int DENIED_UNSPECIFIED = -99;
 
-	private List<MessageListener> mListeners;
+	protected List<MessageListener> mListeners;
 	/** The messages being transferred with msgID_hostName keys */
 	private HashMap<String, Message> incomingMessages;
 	/** The messages this router is carrying */
 	private HashMap<String, Message> messages;
 	/** The messages this router has received as the final recipient */
-	private HashMap<String, Message> deliveredMessages;
+	protected HashMap<String, Message> deliveredMessages;
 	/** The messages that Applications on this router have blacklisted */
-	private HashMap<String, Object> blacklistedMessages;
+	protected HashMap<String, Object> blacklistedMessages;
 	/** Host where this router belongs to */
 	private DTNHost host;
 	/** size of the buffer */
